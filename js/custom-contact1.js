403500e4129716b3f0373c070092fc8a9f7a2b39
Ben Halverson <benhalverson33@gmail.com>
2015-07-14 15:41:59 -0700

    fixed homepage effect

--- js/custom-contact1.js
+++ js/custom-contact1.js
@@ -1,8 +1,8 @@
 (function($) { "use strict";
 
-
-	//Home Sections fit screen
-
+	
+	//Home Sections fit screen	
+				
 	$(function(){"use strict";
 		$('.contact-top, #cd-google-map').css({'height':($(window).height())+'px'});
 		$(window).resize(function(){
@@ -10,179 +10,175 @@
 		});
 	});
 
-
+	
 	//Home Scroll
 
-	jQuery(document).ready(function($){
-		//change this value if you want to change the speed of the scale effect
-		var	scaleSpeed = 0.3,
-		//change this value if you want to set a different initial opacity for the .cd-half-block
-			boxShadowOpacityInitialValue = 0.7,
-			animating = false;
+jQuery(document).ready(function($){
+	//change this value if you want to change the speed of the scale effect
+	var	scaleSpeed = 0.3,
+	//change this value if you want to set a different initial opacity for the .cd-half-block
+		boxShadowOpacityInitialValue = 0.7,
+		animating = false; 
+	
+	//check the media query 
+	var MQ = window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+	$(window).on('resize', function(){
+		MQ = window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+	});
 
-		//check the media query
-		var MQ = window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "");
-		$(window).on('resize', function(){
-			MQ = window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "");
-		});
-
-		//bind the animation to the window scroll event
+	//bind the animation to the window scroll event
+	triggerAnimation();
+	$(window).on('scroll', function(){
 		triggerAnimation();
-		$(window).on('scroll', function(){
-			triggerAnimation();
-		});
+	});
 
-		//move to next/previous section
-		$('.cd-vertical-nav .cd-prev').on('click', function(){
+	//move to next/previous section
+    $('.cd-vertical-nav .cd-prev').on('click', function(){
+    	prevSection();
+    });
+    $('.cd-vertical-nav .cd-next').on('click', function(){
+    	nextSection();
+    });
+    $(document).keydown(function(event){
+		if( event.which=='38' ) {
 			prevSection();
-		});
-		$('.cd-vertical-nav .cd-next').on('click', function(){
+			event.preventDefault();
+		} else if( event.which=='40' ) {
 			nextSection();
-		});
-		$(document).keydown(function(event){
-			if( event.which=='38' ) {
-				prevSection();
-				event.preventDefault();
-			} else if( event.which=='40' ) {
-				nextSection();
-				event.preventDefault();
-			}
-		});
-
-		function triggerAnimation(){
-			if(MQ == 'desktop') {
-				//if on desktop screen - animate sections
-				(!window.requestAnimationFrame) ? animateSection() : window.requestAnimationFrame(animateSection);
-			} else {
-				//on mobile - remove the style added by jQuery
-				$('.cd-section').find('.cd-block').removeAttr('style').find('.cd-half-block').removeAttr('style');
-			}
-			//update navigation arrows visibility
-			checkNavigation();
-		}
-
-		function animateSection () {
-			var scrollTop = $(window).scrollTop(),
-				windowHeight = $(window).height(),
-				windowWidth = $(window).width();
-
-			$('.cd-section').each(function(){
-				var actualBlock = $(this),
-					offset = scrollTop - actualBlock.offset().top,
-					scale = 1,
-					translate = windowWidth/2+'px',
-					opacity,
-					boxShadowOpacity;
-
-				if( offset >= -windowHeight && offset <= 0 ) {
-					//move the two .cd-half-block toward the center - no scale/opacity effect
-					scale = 1,
-					opacity = 1,
-					translate = (windowWidth * 0.5 * (- offset/windowHeight)).toFixed(0)+'px';
-
-				} else if( offset > 0 && offset <= windowHeight ) {
-					//the two .cd-half-block are in the center - scale the .cd-block element and reduce the opacity
-					translate = 0+'px',
-					scale = (1 - ( offset * scaleSpeed/windowHeight)).toFixed(5),
-					opacity = ( 1 - ( offset/windowHeight) ).toFixed(5);
-
-				} else if( offset < -windowHeight ) {
-					//section not yet visible
-					scale = 1,
-					translate = windowWidth/2+'px',
-					opacity = 1;
-
-				} else {
-					//section not visible anymore
-					opacity = 0;
-				}
-
-				boxShadowOpacity = parseInt(translate.replace('px', ''))*boxShadowOpacityInitialValue/20;
-
-				//translate/scale section blocks
-				scaleBlock(actualBlock.find('.cd-block'), scale, opacity);
-
-				var directionFirstChild = ( actualBlock.is(':nth-of-type(even)') ) ? '-': '+';
-				var directionSecondChild = ( actualBlock.is(':nth-of-type(even)') ) ? '+': '-';
-				if(actualBlock.find('.cd-half-block')) {
-					translateBlock(actualBlock.find('.cd-half-block').eq(0), directionFirstChild+translate, boxShadowOpacity);
-					translateBlock(actualBlock.find('.cd-half-block').eq(1), directionSecondChild+translate, boxShadowOpacity);
-				}
-				//this is used to navigate through the sections
-				( offset >= 0 && offset < windowHeight ) ? actualBlock.addClass('is-visible') : actualBlock.removeClass('is-visible');
-			});
-		}
-
-		function translateBlock(elem, value, shadow) {
-			var position = Math.ceil(Math.abs(value.replace('px', '')));
-
-			if( position >= $(window).width()/2 ) {
-				shadow = 0;
-			} else if ( position > 20 ) {
-				shadow = boxShadowOpacityInitialValue;
-			}
-
-			elem.css({
-				'-moz-transform': 'translateX(' + value + ')',
-				'-webkit-transform': 'translateX(' + value + ')',
-				'-ms-transform': 'translateX(' + value + ')',
-				'-o-transform': 'translateX(' + value + ')',
-				'transform': 'translateX(' + value + ')',
-				'box-shadow' : '0px 0px 40px rgba(0,0,0,'+shadow+')'
-			});
-		}
-
-		function scaleBlock(elem, value, opac) {
-			elem.css({
-				'-moz-transform': 'scale(' + value + ')',
-				'-webkit-transform': 'scale(' + value + ')',
-				'-ms-transform': 'scale(' + value + ')',
-				'-o-transform': 'scale(' + value + ')',
-				'transform': 'scale(' + value + ')',
-				'opacity': opac
-			});
-		}
-
-		function nextSection() {
-			if (!animating) {
-				if ($('.cd-section.is-visible').next().length > 0) smoothScroll($('.cd-section.is-visible').next());
-			}
-		}
-
-		function prevSection() {
-			if (!animating) {
-				var prevSection = $('.cd-section.is-visible');
-				if(prevSection.length > 0 && $(window).scrollTop() != prevSection.offset().top) {
-					smoothScroll(prevSection);
-				} else if(prevSection.prev().length > 0 && $(window).scrollTop() == prevSection.offset().top) {
-					smoothScroll(prevSection.prev('.cd-section'));
-				}
-			}
-		}
-
-		function checkNavigation() {
-			( $(window).scrollTop() < $(window).height()/2 ) ? $('.cd-vertical-nav .cd-prev').addClass('inactive') : $('.cd-vertical-nav .cd-prev').removeClass('inactive');
-			( $(window).scrollTop() < $(window).height()/2 ) ? $('.cd-vertical-nav .cd-next').addClass('inactive1') : $('.cd-vertical-nav .cd-next').removeClass('inactive1');
-			( $(window).scrollTop() > $(document).height() - 3*$(window).height()/2 ) ? $('.cd-vertical-nav .cd-next').addClass('inactive') : $('.cd-vertical-nav .cd-next').removeClass('inactive');
-		}
-
-
-
-		function smoothScroll(target) {
-			animating = true;
-			$('body,html').animate({'scrollTop': target.offset().top}, 500, function(){ animating = false; });
+			event.preventDefault();
 		}
 	});
 
+	function triggerAnimation(){
+		if(MQ == 'desktop') {
+			//if on desktop screen - animate sections
+			(!window.requestAnimationFrame) ? animateSection() : window.requestAnimationFrame(animateSection);
+		} else {
+			//on mobile - remove the style added by jQuery 
+			$('.cd-section').find('.cd-block').removeAttr('style').find('.cd-half-block').removeAttr('style');
+		}
+		//update navigation arrows visibility
+		checkNavigation();
+	}
+	
+	function animateSection () {
+		var scrollTop = $(window).scrollTop(),
+			windowHeight = $(window).height(),
+			windowWidth = $(window).width();
+		
+		$('.cd-section').each(function(){
+			var actualBlock = $(this),
+				offset = scrollTop - actualBlock.offset().top,
+				scale = 1,
+				translate = windowWidth/2+'px',
+				opacity,
+				boxShadowOpacity;
 
-	//Navigation
+			if( offset >= -windowHeight && offset <= 0 ) {
+				//move the two .cd-half-block toward the center - no scale/opacity effect
+				scale = 1,
+				opacity = 1,
+				translate = (windowWidth * 0.5 * (- offset/windowHeight)).toFixed(0)+'px';
+
+			} else if( offset > 0 && offset <= windowHeight ) {
+				//the two .cd-half-block are in the center - scale the .cd-block element and reduce the opacity
+				translate = 0+'px',
+				scale = (1 - ( offset * scaleSpeed/windowHeight)).toFixed(5),
+				opacity = ( 1 - ( offset/windowHeight) ).toFixed(5);
+
+			} else if( offset < -windowHeight ) {
+				//section not yet visible
+				scale = 1,
+				translate = windowWidth/2+'px',
+				opacity = 1;
+
+			} else {
+				//section not visible anymore
+				opacity = 0;
+			}
+			
+			boxShadowOpacity = parseInt(translate.replace('px', ''))*boxShadowOpacityInitialValue/20;
+			
+			//translate/scale section blocks
+			scaleBlock(actualBlock.find('.cd-block'), scale, opacity);
+
+			var directionFirstChild = ( actualBlock.is(':nth-of-type(even)') ) ? '-': '+';
+			var directionSecondChild = ( actualBlock.is(':nth-of-type(even)') ) ? '+': '-';
+			if(actualBlock.find('.cd-half-block')) {
+				translateBlock(actualBlock.find('.cd-half-block').eq(0), directionFirstChild+translate, boxShadowOpacity);
+				translateBlock(actualBlock.find('.cd-half-block').eq(1), directionSecondChild+translate, boxShadowOpacity);	
+			}
+			//this is used to navigate through the sections
+			( offset >= 0 && offset < windowHeight ) ? actualBlock.addClass('is-visible') : actualBlock.removeClass('is-visible');		
+		});
+	}
+
+	function translateBlock(elem, value, shadow) {
+		var position = Math.ceil(Math.abs(value.replace('px', '')));
+		
+		if( position >= $(window).width()/2 ) {
+			shadow = 0;	
+		} else if ( position > 20 ) {
+			shadow = boxShadowOpacityInitialValue;
+		}
+
+		elem.css({
+		    '-moz-transform': 'translateX(' + value + ')',
+		    '-webkit-transform': 'translateX(' + value + ')',
+			'-ms-transform': 'translateX(' + value + ')',
+			'-o-transform': 'translateX(' + value + ')',
+			'transform': 'translateX(' + value + ')',
+			'box-shadow' : '0px 0px 40px rgba(0,0,0,'+shadow+')'
+		});
+	}
+
+	function scaleBlock(elem, value, opac) {
+		elem.css({
+		    '-moz-transform': 'scale(' + value + ')',
+		    '-webkit-transform': 'scale(' + value + ')',
+			'-ms-transform': 'scale(' + value + ')',
+			'-o-transform': 'scale(' + value + ')',
+			'transform': 'scale(' + value + ')',
+			'opacity': opac
+		});
+	}
+
+	function nextSection() {
+		if (!animating) {
+			if ($('.cd-section.is-visible').next().length > 0) smoothScroll($('.cd-section.is-visible').next());
+		}
+	}
+
+	function prevSection() {
+		if (!animating) {
+			var prevSection = $('.cd-section.is-visible');
+			if(prevSection.length > 0 && $(window).scrollTop() != prevSection.offset().top) {
+				smoothScroll(prevSection);
+			} else if(prevSection.prev().length > 0 && $(window).scrollTop() == prevSection.offset().top) {
+				smoothScroll(prevSection.prev('.cd-section'));
+			}
+		}
+	}
+
+	function checkNavigation() {
+		( $(window).scrollTop() < $(window).height()/2 ) ? $('.cd-vertical-nav .cd-prev').addClass('inactive') : $('.cd-vertical-nav .cd-prev').removeClass('inactive');
+		( $(window).scrollTop() > $(document).height() - 3*$(window).height()/2 ) ? $('.cd-vertical-nav .cd-next').addClass('inactive') : $('.cd-vertical-nav .cd-next').removeClass('inactive');
+	}
+
+	function smoothScroll(target) {
+		animating = true;
+        $('body,html').animate({'scrollTop': target.offset().top}, 500, function(){ animating = false; });
+	}
+});
+	
+	//Navigation	
 
 	$('ul.slimmenu').on('click',function(){
-	var width = $(window).width();
-	if ((width <= 1200)){
-		$(this).slideToggle();
-	}
-	});
+	var width = $(window).width(); 
+	if ((width <= 1200)){ 
+		$(this).slideToggle(); 
+	}	
+	});				
 	$('ul.slimmenu').slimmenu(
 	{
 		resizeWidth: '1200',
@@ -191,8 +187,8 @@
 		animSpeed:'medium',
 		indentChildren: true,
 		childrenIndenter: '&raquo;'
-	});
-
+	});	
+	
 	$(document).ready(function(){"use strict";
 		$(".scroll").click(function(event){
 
@@ -208,34 +204,34 @@
 		});
 	});
 	
-
+	
 	//set your google maps parameters
 
 	jQuery(document).ready(function($){
-
-		var latitude = 37.42135,
-			longitude = -121.958456,
+		
+		var latitude = 44.8013716,
+			longitude = 20.4631372,
 			map_zoom = 15;
 
 		//google map custom marker icon - .png fallback for IE11
 		var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
 		var marker_url = ( is_internetExplorer11 ) ? 'images/cd-icon-location.png' : 'images/cd-icon-location.svg';
-
+			
 		//define the basic color of your map, plus a value for saturation and brightness
 		var	main_color = '#cccccc',
 			saturation_value= -100,
 			brightness_value= 9;
 
 		//we define here the style of the map
-		var style= [
+		var style= [ 
 			{
 				//set saturation for the labels on the map
 				elementType: "labels",
 				stylers: [
 					{saturation: saturation_value}
 				]
-			},
-			{	//poi stands for point of interest - don't show these lables on the map
+			},  
+			{	//poi stands for point of interest - don't show these lables on the map 
 				featureType: "poi",
 				elementType: "labels",
 				stylers: [
@@ -249,22 +245,22 @@
 				stylers: [
 					{visibility: "off"}
 				]
-			},
-			{
+			}, 
+			{ 	
 				//don't show local road lables on the map
-				featureType: "road.local",
-				elementType: "labels.icon",
+				featureType: "road.local", 
+				elementType: "labels.icon", 
 				stylers: [
-					{visibility: "off"}
-				]
+					{visibility: "off"} 
+				] 
 			},
-			{
+			{ 
 				//don't show arterial road lables on the map
-				featureType: "road.arterial",
-				elementType: "labels.icon",
+				featureType: "road.arterial", 
+				elementType: "labels.icon", 
 				stylers: [
 					{visibility: "off"}
-				]
+				] 
 			},
 			{
 				//don't show road lables on the map
@@ -273,25 +269,25 @@
 				stylers: [
 					{visibility: "off"}
 				]
-			},
+			}, 
 			//style different elements on the map
-			{
-				featureType: "transit",
-				elementType: "geometry.fill",
+			{ 
+				featureType: "transit", 
+				elementType: "geometry.fill", 
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
-			},
+			}, 
 			{
 				featureType: "poi",
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -300,8 +296,8 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -310,8 +306,8 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -320,8 +316,8 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -330,8 +326,8 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -340,8 +336,8 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -350,8 +346,8 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -359,19 +355,19 @@
 				featureType: "landscape",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
-
+				
 			},
 			{
 				featureType: "road",
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			},
@@ -380,23 +376,23 @@
 				elementType: "geometry.fill",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
-			},
+			}, 
 			{
 				featureType: "water",
 				elementType: "geometry",
 				stylers: [
 					{ hue: main_color },
-					{ visibility: "on" },
-					{ lightness: brightness_value },
+					{ visibility: "on" }, 
+					{ lightness: brightness_value }, 
 					{ saturation: saturation_value }
 				]
 			}
 		];
-
+			
 		//set google map options
 		var map_options = {
 			center: new google.maps.LatLng(latitude, longitude),
@@ -411,7 +407,7 @@
 		}
 		//inizialize the map
 		var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-		//add a custom marker to the map
+		//add a custom marker to the map				
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(latitude, longitude),
 			map: map,
@@ -421,7 +417,7 @@
 
 		//add custom buttons for the zoom-in/zoom-out on the map
 		function CustomZoomControl(controlDiv, map) {
-			//grap the zoom elements from the DOM and insert them in the map
+			//grap the zoom elements from the DOM and insert them in the map 
 			var controlUIzoomIn= document.getElementById('cd-zoom-in'),
 				controlUIzoomOut= document.getElementById('cd-zoom-out');
 			controlDiv.appendChild(controlUIzoomIn);
@@ -445,5 +441,39 @@
 
 
 
+	
+  })(jQuery); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
-  })(jQuery);
+
+
+
+
+	
